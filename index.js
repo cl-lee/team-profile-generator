a846d40d5e6ce13b8bb8b61fe4d8450f021c1f81
cl.lee <cl.lee.ofz@gmail.com>
2023-02-24 19:57:12 +0000

    added inquirer codes for obtaining manager's info in index.js

--- index.js
+++ index.js
@@ -3,27 +3,61 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// 1st gather info
-// with Inquirer eventually
-
-let newEngineer = new Engineer("Mana", 123, "test@test.com", "cl-lee");
+// Gathers employee information using Inquirer
 
 let team = [];
-team.push(newEngineer);
 
-// 2nd put info onto page template (from .\src\page-template.js)
-let renderHTMLDocument = render(team);
+async function initiateApplication() {
+  // Inquirer prompts
+  let { managerName, employeeID, emailAddress, officeNumber } =
+    await inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Please enter the team manager's NAME:",
+      },
+      {
+        type: "input",
+        name: "employeeID",
+        message: "Please enter the team manager's EMPLOYEE ID:",
+      },
+      {
+        type: "input",
+        name: "emailAddress",
+        message: "Please enter the team manager's EMAIL ADDRESS:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter the team manager's OFFICE NUMBER:",
+      },
+    ]);
 
-// 3rd generate output (fs writeFile)
-fs.writeFile(outputPath, renderHTMLDocument, (err) =>
-err ? console.error(err) : console.log('Success!'));
+  let newManager = new Manager(
+    managerName,
+    employeeID,
+    emailAddress,
+    officeNumber
+  );
+
+  team.push(newManager);
+  
+  // 2nd put info onto page template (from .\src\page-template.js)
+  let renderHTMLDocument = render(team);
+
+  // 3rd generate output (fs writeFile)
+  fs.writeFile(outputPath, renderHTMLDocument, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
+
+initiateApplication();
